79bf3b40a779157ef423a7991eeae6a0eb177804
sammed.sankonatti <sammed.sankonatti18@gmail.com>
2025-03-20 17:46:56 +0530

    code for cms

---
+++ src/app/results/layout.js
@@ -0,0 +1,25 @@
+"use client";
+import { Box } from "@mui/material";
+import React from "react";
+import { NavBar } from "../components";
+import GuideBreadcrumbs from "../components/breadcrumbs/SearchBreadcrumbs";
+
+const ResultsLayout = ({ children }) => {
+  return (
+    <Box>
+      <NavBar />
+
+      <Box sx={{ mt: 8, p: 3 }}>
+        <GuideBreadcrumbs
+          items={[
+            { label: "DrillBit Guides", href: "/" },
+            { label: "Search Results" }, // Current page (non-clickable)
+          ]}
+        />
+        {children}
+      </Box>
+    </Box>
+  );
+};
+
+export default ResultsLayout;
