9e92137e21df374a8fdd8c144fa09e016583d921
sammed.sankonatti <sammed.sankonatti18@gmail.com>
2025-03-27 13:07:19 +0530

    web config and script updated!

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const { createServer } = require("http");
 const next = require("next");
 
 const port = process.env.PORT || 3000;
-const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
+const app = next({ dev: false });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
